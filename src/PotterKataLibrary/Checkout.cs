de15ee91a8a55eefd131fe7bcd44009ab4af2f9e
Philip Devine <Devine33@live.co.uk>
2021-02-21 17:02:08 +0000

    Minor refactoring and renamed initial test file

---
+++ src/PotterKataLibrary/Checkout.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PotterKataLibrary
+{
+    public class Checkout
+    {
+        private readonly Basket _customerBasket;
+        private List<List<string>> _discountGroups = new();
+
+        public Checkout(Basket basket)
+        {
+            _customerBasket = basket;
+        }
+
+
+        public decimal CompletePurchase()
+        {
+            
+            var standardDiscount = CalculateStandardTotal();
+            var groupDiscount = CalculateGroupTotal();
+
+           return  groupDiscount > standardDiscount ? groupDiscount : standardDiscount;
+        }
+
+        public decimal CalculateTotal()
+        {
+            var total = _customerBasket.TotalNumberOfItems * 8.00m;
+            return total;
+        }
+
+        //Basic discount is based on total number of unique books purchased
+        public decimal CalculateStandardTotal()
+        {
+            var discount = 0.0m;
+            var uniqueBooks = _customerBasket.Books.GroupBy(x => x.Name).Distinct().Count();
+            switch (uniqueBooks)
+            {
+                case 0:
+                    discount =  0.0m;
+                    break;
+                case 1:
+                    discount =  0.0m;
+                    break;
+                case 2:
+                    discount =  0.05m;
+                    break;
+                case 3:
+                    discount =  0.10m;
+                    break;
+                case 4:
+                    discount = 0.15m;
+                    break;
+                case 5:
+                    discount = 0.25m;
+                    break;
+                case 6:
+                    discount = 0.30m;
+                    break;
+                case 7:
+                    discount = 0.35m;
+                    break;
+            }
+            var total = CalculateTotal();
+            var totalDiscount = total * discount;
+            var finalPurchaseCost = total - totalDiscount;
+            return finalPurchaseCost;
+        }
+
+        public decimal CalculateGroupTotal()
+        {
+            CalculateDiscountGroups();
+
+            var totalCost = 0.0m;
+            foreach (var discountList in _discountGroups)
+            {
+                var total = discountList.Count * 8.00m;
+                var discount = 0.0m;
+                var disList = discountList.Count;
+                switch (disList) //todo: Abstract to classes or own function
+                {
+                    case 0:
+                        discount += 0.0m;
+                        break;
+                    case 1:
+                        discount += 0.0m;
+                        break;
+                    case 2:
+                        discount += 0.05m;
+                        break;
+                    case 3:
+                        discount += 0.10m;
+                        break;
+                    case 4:
+                        discount += 0.15m;
+                        break;
+                    case 5:
+                        discount += 0.25m;
+                        break;
+                    case 6:
+                        discount += 0.30m;
+                        break;
+                    case 7:
+                        discount += 0.35m;
+                        break;
+                }
+
+                var totalDiscount = total * discount;
+                var finalPurchaseCost = total - totalDiscount;
+                totalCost += finalPurchaseCost;
+            }
+          
+
+            return totalCost;
+        }
+
+        public void CalculateDiscountGroups()
+        {
+            var discountLists = new List<List<string>>();
+
+            foreach (var customerBook in _customerBasket.Books)
+            {
+                var listNotContainingBook = discountLists.Any(x => !x.Contains(customerBook.Name));
+                if (listNotContainingBook) // if a list dosent contains a book add it
+                {
+                    var list = discountLists.FirstOrDefault(x => !x.Contains(customerBook.Name));
+                    list.Add(customerBook.Name);
+                }
+                else
+                {
+                    discountLists.Add(new List<string>() {customerBook.Name});
+                }
+            }
+
+            _discountGroups = discountLists;
+        }
+    }
+}
---
+++ tests/UnitTests/PotterKataLibraryTests/PotterKataLibraryTests.cs
@@ -0,0 +1,141 @@
+﻿using NUnit.Framework;
+using PotterKataLibrary;
+
+namespace PotterKataLibraryTests
+{
+    public class PotterKataLibraryTests
+    {
+        private Basket _basket;
+        private Checkout _checkout;
+
+        [SetUp]
+        public void Setup()
+        {
+            _basket = new Basket();
+        }
+
+        [Test]
+        public void AddBook_UpdateBasketTotal()
+        {
+            var bookToAdd = new Book {Name = "Phil .Stone"};
+
+            _basket.AddBook(bookToAdd);
+            Assert.AreEqual(1, _basket.TotalNumberOfItems);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesNoDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOfBooks_NoDiscount());
+            var total = _checkout.CalculateStandardTotal();
+            Assert.AreEqual(40m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesFivePercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_FivePercentDiscount());
+            var total = _checkout.CalculateStandardTotal();
+            Assert.AreEqual(15.2m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTenPercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_TenPercentDiscount());
+            var discount = _checkout.CalculateStandardTotal();
+            Assert.AreEqual(21.6m, discount);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesFifteenPercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_FifteenPercentDiscount());
+            var total = _checkout.CalculateStandardTotal();
+            Assert.AreEqual(27.2, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTwentyFivePercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_TwentyFivePercentDiscount());
+            var total = _checkout.CalculateStandardTotal();
+            Assert.AreEqual(30.0m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesThirtyPercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_ThirtyPercentDiscount());
+            var discount = _checkout.CalculateStandardTotal();
+            Assert.AreEqual(33.6m, discount);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesThirtyFivePercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_ThirtyFivePercentDiscount());
+            var discount = _checkout.CalculateStandardTotal();
+            Assert.AreEqual(36.4m, discount);
+        }
+
+
+        //TOTAL COST TESTS
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTotalCostWithNoDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOfBooks_NoDiscount());
+            var total = _checkout.CompletePurchase();
+            Assert.AreEqual(40m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTotalCostWithFivePercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_FivePercentDiscount());
+            var total = _checkout.CompletePurchase();
+            Assert.AreEqual(15.2m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTotalCostWithTenPercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_TenPercentDiscount());
+            var total = _checkout.CompletePurchase();
+            Assert.AreEqual(21.6m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTotalCostWithFifteenPercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_FifteenPercentDiscount());
+            var total = _checkout.CompletePurchase();
+            Assert.AreEqual(27.2m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTotalCostWithTwentyFivePercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_TwentyFivePercentDiscount());
+            var total = _checkout.CompletePurchase();
+            Assert.AreEqual(30.0m, total);
+        }
+
+        [Test]
+        public void Checkout_CorrectlyCalculatesTotalCostWithThirtyPercentDiscount()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOFBooks_ThirtyPercentDiscount());
+            var total = _checkout.CompletePurchase();
+            Assert.AreEqual(33.6m, total);
+        }
+
+       [Test]
+        public void Checkout_TestScenario()
+        {
+            _checkout = new Checkout(TestHelper.BasketFullOfBooks_TestScenario());
+            var total = _checkout.CompletePurchase();
+            Assert.AreEqual(60.80m, total);
+        }
+    }
+}
